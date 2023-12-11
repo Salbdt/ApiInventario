327edce24b3c093463ca29c5a5ddfb4d51254b7d
Santi <slopez@bdtglobal.com>
2023-12-11 12:03:13 -0300

    IBaseRepository

---
+++ Inventory.Persistence/Interfaces/IBaseRepository.cs
@@ -0,0 +1,13 @@
+namespace Inventory.Persistence.Interfaces
+{
+    public interface IBaseRepository<T> where T: class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync();
+        Task<T> AddAsync(T entity);
+        Task<IEnumerable<T>> AddAll(IEnumerable<T> entities);
+        Task<bool> UpdateAsync(int id, T entity);
+        Task<bool> DeleteAsync(int id);
+        Task<bool> DeleteAsync(T TEntity);
+    }
+}
