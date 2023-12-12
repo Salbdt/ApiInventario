26c425843938e0daa8c6d2995538f6219b5d2f9e
Santi <slopez@bdtglobal.com>
2023-12-12 14:29:42 -0300

    Inventory.DTOs y CategoryDTO

---
+++ Inventory.DTOs/Category/CategoryToCreateDTO.cs
@@ -0,0 +1,8 @@
+namespace Inventory.DTOs.Category
+{
+    public class CategoryToCreateDTO
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
---
+++ Inventory.DTOs/Category/CategoryToListDTO.cs
@@ -0,0 +1,11 @@
+namespace Inventory.DTOs.Category
+{
+    public class CategoryToListDTO
+    {        
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
