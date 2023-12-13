47b00784c6aa97dba4f25bb784135f61d2c7a159
Santi <slopez@bdtglobal.com>
2023-12-13 17:46:58 -0300

    Program DependencyContainer - AddScoped

--- Inventory.APIAuthorization/Program.cs
+++ Inventory.APIAuthorization/Program.cs
@@ -1,9 +1,12 @@
 using Inventory.Persistence;
+using Inventory.Persistence.Interfaces;
+using Inventory.Persistence.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddAuthContextSQLServer(builder.Configuration, "DefaultConnection");
+builder.Services.AddScoped<IAuthRepository, AuthRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
