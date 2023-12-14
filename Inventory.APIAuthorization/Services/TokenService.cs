edb846ab1f99b3848b0d775ef506b11ada36e9c8
Santi <slopez@bdtglobal.com>
2023-12-14 13:59:50 -0300

    TokenService CreateToken

--- Inventory.APIAuthorization/Services/TokenService.cs
+++ Inventory.APIAuthorization/Services/TokenService.cs
@@ -1,13 +1,45 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Inventory.APIAuthorization.Services.Interfaces;
 using Inventory.Entities;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Inventory.APIAuthorization.Services
 {
     public class TokenService : ITokenService
     {
+        private readonly SymmetricSecurityKey _sskey;
+
+        public TokenService(IConfiguration configuration)
+        {
+            _sskey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(configuration["tokenKey"]));
+        }
+
         public string CreateToken(User user)
         {
-            return "";
+            var claims = new List<Claim>();
+            claims.Add(new Claim(JwtRegisteredClaimNames.NameId, user.Email));
+            claims.Add(new Claim("Id", user.Id.ToString()));
+            claims.Add(new Claim("Name", user.Name));
+            claims.Add(new Claim("Email", user.Email));
+
+            var credentials = new SigningCredentials(_sskey, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Issuer = "issuer",
+                Audience = "issuer",
+                Subject = new(claims),
+                SigningCredentials = credentials,
+                Expires = DateTime.UtcNow.AddHours(1)
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
